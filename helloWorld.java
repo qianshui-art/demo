7dff02066d48202f51d57cdb07abeb3c677b9c21
qianshui-art <zhoufei9966@live.com>
2019-11-27 17:11:37 +0800

    hello world

---
+++ helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld{
+
+	public static void main(String[] args){
+		System.out.println("helloWorld");
+		System.out.println("1");
+		System.out.println("helloWorld");
+		System.out.println("2");
+	}
+}
